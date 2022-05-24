df6365b22c88c4a57f62d1a3e5f2d33ba2447743
McKay <wmsmckay@gmail.com>
2022-05-24 09:57:18 -0600

    formatting

--- src/js/main.js
+++ src/js/main.js
@@ -7,17 +7,15 @@ flyInHeaderFooter();
 
 const externalServices = new ExternalServices()
 
-
+// Gets a book by id and returns it's promise
 function getBookById() {
     let book = externalServices.findBookById("Prj1iTmPJn4C");
     console.log(book)
 }
 
-
+//  Gets a list of 10 books in an array 
 function searchBar() {
     const search = document.querySelector("#searchBar");
-    // console.log(search)
-
     search.addEventListener("search", () => {
         let results = externalServices.getSearchResults(search.value)
         console.log(results)
